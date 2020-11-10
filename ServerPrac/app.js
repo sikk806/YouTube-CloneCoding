f07ed645b4040c1c131ac8cb88062ccf76104fcc
sikk806 <sikk806@gmail.com>
2020-11-10 16:36:45 +0900

    set Router

--- ServerPrac/app.js
+++ ServerPrac/app.js
@@ -4,24 +4,20 @@ import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./Routers/videoRouter";
+import globalRouter from "./Routers/globalRouter";
 
 const app = express();
-
-const handleHome = (req, res) => res.send("도구리 응애");
-
-const handleProfile = (req, res) => res.send("도구리 메롱");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( { extended: true }));
 app.use(helmet()); // For Security
-app.use(morgan("combined"));
+app.use(morgan("dev"));
 
-
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
+app.use("/", globalRouter);
 app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 export default app;
