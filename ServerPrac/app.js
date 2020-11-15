ff358405e2183abb4ad9b02087c1363a96fa9a32
sikk806 <sikk806@gmail.com>
2020-11-15 21:10:59 +0900

    Controller

--- ServerPrac/app.js
+++ ServerPrac/app.js
@@ -7,6 +7,7 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./Routers/videoRouter";
 import globalRouter from "./Routers/globalRouter";
 import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 
@@ -16,6 +17,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( { extended: true }));
 app.use(helmet()); // For Security
 app.use(morgan("dev"));
+
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
