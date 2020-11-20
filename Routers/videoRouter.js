2b9e550559df5d340e9b6bbbb99538380023042d
sikk806 <sikk806@gmail.com>
2020-11-20 22:26:05 +0900

    set middleware

--- Routers/videoRouter.js
+++ Routers/videoRouter.js
@@ -1,11 +1,14 @@
 import express from "express";
 import routes from "../routes";
-import { getUpload, postUpload, videoDetail, editVideo, deleteVideo } from "../controllers/videoControllers"
+import { getUpload, postUpload, videoDetail, editVideo, deleteVideo } from "../controllers/videoControllers";
+import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
 videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, postUpload);
+// file을 Upload하면 server에 있는 folder('video/' in middleware.js)에 Upload
+// postUpload는 해당 file로 접근하지만, file이 아닌 URL 형태로 접근하게 된다.
+videoRouter.post(routes.upload, uploadVideo, postUpload);
 
 videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
--- middlewares.js
+++ middlewares.js
@@ -1,4 +1,8 @@
+// multer : function을 넣으면 middleware가 file의 URL을 반환시켜줌.
+import multer from "multer"
 import routes from "./routes"
+
+export const multerVideo = multer({dest: "videos/"});
 
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "WeTube";
@@ -9,3 +13,7 @@ export const localsMiddleware = (req, res, next) => {
     };
     next();
 };
+
+// single : 웹 페이지에서 업로드를 눌렀을 때 오직 하나의 파일만 업로드 가능 
+// ' ' 안에는 upload에서 File의 이름이 들어오게 된다. (name='file')
+export const uploadVideo = multerVideo.single.("videoFile");
