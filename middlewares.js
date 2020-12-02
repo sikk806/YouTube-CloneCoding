4928a156d72b170d5b588cfb9194c20ffb288165
sikk806 <sikk806@gmail.com>
2020-12-02 21:00:34 +0900

    header, footer, form

--- middlewares.js
+++ middlewares.js
@@ -1,19 +1,19 @@
 // multer : function을 넣으면 middleware가 file의 URL을 반환시켜줌.
-import multer from "multer"
-import routes from "./routes"
+import multer from "multer";
+import routes from "./routes";
 
-export const multerVideo = multer({dest: "uploads/videos/"});
+export const multerVideo = multer({ dest: "uploads/videos/" });
 
 export const localsMiddleware = (req, res, next) => {
-    res.locals.siteName = "WeTube";
-    res.locals.routes = routes;
-    res.locals.user = {
-        isAuthenticated: true,
-        id: 1
-    };
-    next();
+  res.locals.siteName = "WeTube";
+  res.locals.routes = routes;
+  res.locals.user = {
+    isAuthenticated: false,
+    id: 1,
+  };
+  next();
 };
 
-// single : 웹 페이지에서 업로드를 눌렀을 때 오직 하나의 파일만 업로드 가능 
+// single : 웹 페이지에서 업로드를 눌렀을 때 오직 하나의 파일만 업로드 가능
 // ' ' 안에는 upload에서 File의 이름이 들어오게 된다. (name='file')
 export const uploadVideo = multerVideo.single("videoFile");
